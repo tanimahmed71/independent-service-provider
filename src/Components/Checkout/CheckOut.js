66631724eebce1cfb4e5323d4118ee6e5b1d70d0
tanimahmed71 <ahmedtanim838@gmail.com>
2022-05-07 21:30:58 +0600

    checkout part half complet

--- src/Components/Checkout/CheckOut.js
+++ src/Components/Checkout/CheckOut.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import "./Checkout.css";
 
 const CheckOut = () => {
+    const handleBooking =(event)=>{
+        event.preventDefault();
+        console.log('lkfjkdfjkdj');
+    }
     return (
         <div>
-            <h4>Let's  get check and out</h4>
+                    <h5>CONTACT US</h5>
+                    <h2>Let's booking</h2>
+            <div className='checkout-container'>
+            <div>
+                <img style={{width:"100%"}} src="https://i.ibb.co/CwmZ8k9/cover-2048.jpg" alt="" />
+            </div>
+            <div>
+                <form>
+                    <input type="text" placeholder='User Name' required />
+                    <hr />
+                    <input type="email" name="email address" placeholder='Enter email' id="" required />
+                    <hr />
+                    <input type="text" name="number" placeholder='Phone Number' id="" required/>
+                    <hr />
+                    <input type="text"placeholder='Address'required  />
+                    <hr />
+                    <button onClick={handleBooking}>Booking</button>
+                </form>
+            </div>
+            </div>
         </div>
     );
 };
