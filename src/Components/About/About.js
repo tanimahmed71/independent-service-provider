c4459191d6ca6248995369e35614fb0b61b659c9
tanimahmed71 <ahmedtanim838@gmail.com>
2022-05-07 14:13:09 +0600

    about part commplet

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import "./About.css"
 
 const About = () => {
     return (
         <div>
-        <p>About me</p>
-        
+            <h3>About Me</h3>
+          <div className='about-container'>
+          <div>
+                <img src="https://i.ibb.co/Zd75mvT/01.jpg" alt="" />
+            </div>
+            <div>
+                <p>My name is Tanim ahmed, I learning web development for almost 4.5 months. I know about the 70% of web development. At present, my dream is to complete my web journey with the programming hero team.</p>
+            </div>
+          </div>
         </div>
     );
 };
